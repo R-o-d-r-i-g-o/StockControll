b37608ad40b8e2be00a600307363392a65c08926
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-08-30 16:36:41 -0300

    feat: add login screen

---
+++ ViewModel/LoginViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace StockControll.ViewModel
+{
+    public class UserViewModel
+    {
+
+        [Required(ErrorMessage = "Obrigatório")]
+        [Display(Name = "Nome")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Obrigatório"), DataType(DataType.Password)]
+        [Display(Name = "Senha")]
+        public string Password { get; set; }
+    }
+}
