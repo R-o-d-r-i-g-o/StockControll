b73775b6aa19040328d50cd4d75d03b3e3e4f7fa
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-09-01 00:22:52 -0300

    feat: add user paged list

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,14 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Linq;
 using System.Web.Mvc;
+using PagedList;
+using StockControll.Context;
+using StockControll.ViewModel;
 
 namespace StockControll.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly AppDbContext _db = new AppDbContext();
+
         public ActionResult Index()
         {
             return View();
@@ -21,11 +23,16 @@ namespace StockControll.Controllers
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult Contact(FilterViewModel filters)
         {
+            filters.VerifyFilter();
             ViewBag.Message = "Your contact page.";
 
-            return View();
+            return View(new UsersViewModel
+            {
+                Filters = filters,
+                Users = _db.Users.OrderBy(u => u.Id).ToPagedList(filters.Page, filters.Rows),
+            });
         }
     }
 }
---
+++ ViewModel/FilterViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StockControll.ViewModel
+{
+    public class FilterViewModel
+    {
+        public int Rows { get; set; }
+        public int Page { get; set; }
+        public string SearchItem { get; set; }
+
+        public void VerifyFilter()
+        {
+            if (this.Rows == default)
+                this.Rows = 10;
+
+            if (this.Page == default)
+                this.Page = 1;
+        }
+    }
+}
