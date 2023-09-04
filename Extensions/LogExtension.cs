2a737cd7066c335488d4c02983f1d8131653a815
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-09-04 09:31:29 -0300

    feat: add log extension

---
+++ Extensions/LogExtension.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Web;
+using StockControll.Context;
+using StockControll.Models;
+using static StockControll.Commons.Enums;
+
+namespace StockControll.Extensions
+{
+    public class LogExtension
+    {
+        private readonly AppDbContext _db;
+        private readonly User _loggedUser;
+
+        public LogExtension(AppDbContext _db = null)
+        {
+            this._db = _db ?? new AppDbContext();
+            this._loggedUser = (User)HttpContext.Current.Session["user"];
+        }
+
+        public void AddMessage(ActivityType activityType, string message)
+        {
+            var sessionUser = _db.Users.Find(_loggedUser.Id);
+            if (sessionUser == null)
+                throw new Exception("Usuário da sessão não encontrado");
+
+            _db.Logs.Add(new Log {
+                User = sessionUser,
+                Message = message,
+                ActivityType = activityType,
+            });
+            _db.SaveChanges();
+        }
+    }
+}
