ea73c78d2c3c367b36a37edc90e235c70a4f28c2
Rodrigo <rodrigo.ribeiro@q2pay.com.br>
2023-09-24 16:19:35 -0300

    .

---
+++ ViewModel/CategoryViewModel.cs
@@ -0,0 +1,12 @@
+﻿using PagedList;
+using StockControll.Models;
+
+namespace StockControll.ViewModel
+{
+    public class CategoryViewModel
+    {
+        public FilterViewModel Filters { get; set; }
+        public IPagedList<Category> Categories { get; set; }
+        public Category NewCategory { get; set; }
+    }
+}
